3ee98960f9745f45870572cd2cd5d2a7906d1ed7
Alex Knopach <alexanderknopach@gmail.com>
2021-06-14 21:09:12 +0300

    feat: implement

--- Swift_Task5/ios.stage-task/Exercises/Exercise-1/StockMaximize.swift
+++ Swift_Task5/ios.stage-task/Exercises/Exercise-1/StockMaximize.swift
@@ -1,8 +1,57 @@
 import Foundation
 
 class StockMaximize {
-
+    var max = 0
+    var maxIndex = 0
+    var tempPrices = [Int]()
+    var profit = 0
+    
     func countProfit(prices: [Int]) -> Int {
-        return -1
-    }
+        
+        
+            if prices == [Int]() || prices.count == 0 {
+                return profit
+            }
+            
+            tempPrices = prices
+            removeMaxAtFirstElement(prices: &tempPrices)
+            
+            if tempPrices.count == 1 {
+                return profit
+            }
+            
+            for (index, share) in tempPrices.enumerated() {
+                if share <= max && index < maxIndex {
+                    profit += max - share
+                    tempPrices.removeFirst()
+                }
+            }
+            
+            removeMaxAtFirstElement(prices: &tempPrices)
+            if tempPrices.count >= 2 {
+                countProfit(prices: tempPrices)
+            }
+            
+            return profit
+            
+        }
+        
+        func removeMaxAtFirstElement(prices: inout [Int]) {
+            if prices.count >= 2 {
+                for (index, item) in prices.enumerated() {
+                    if item >= max {
+                        max = item
+                        maxIndex = index
+                    }
+                }
+                
+                if maxIndex == 0 {
+                    max = 0
+                    prices.remove(at: maxIndex)
+                    removeMaxAtFirstElement(prices: &prices)
+                }
+            }
+        }
+        
+    
 }
--- Swift_Task5/ios.stage-task/Exercises/Exercise-2/MessageDecryptor.swift
+++ Swift_Task5/ios.stage-task/Exercises/Exercise-2/MessageDecryptor.swift
@@ -3,6 +3,53 @@ import UIKit
 class MessageDecryptor: NSObject {
     
     func decryptMessage(_ message: String) -> String {
-        return ""
+        
+//        SHAME SHAME SHAME SHAME SHAME
+        
+        
+        var tmpStr = ""
+        var tempIntstr = "0"
+        var tempInt = 0
+        var resultStr = ""
+
+        for chr in message {
+            if chr.isNumber {
+                if tmpStr != "" {
+                    for _ in 0 ..< tempInt {
+                        resultStr.append(tmpStr)
+                    }
+                    tmpStr = ""
+                    tempIntstr = "0"
+                    tempInt = 0
+                }
+                    tempIntstr.append(chr)
+            }
+            if chr == "[" && message.first != "[" {
+                tempInt = Int(tempIntstr)!
+                if tempInt == 0 {
+                    resultStr = ""
+                    break
+                }
+                tempIntstr = "0"
+            }
+            if chr.isLetter {
+                if tempInt == 0 {
+                    tempInt = 1
+                }
+                tmpStr.append(chr)
+            }
+            if chr == "]" {
+                for _ in 0 ..< tempInt {
+                    resultStr.append(tmpStr)
+                }
+                tmpStr = ""
+                tempIntstr = "0"
+                tempInt = 0
+            }
+        }
+
+        resultStr.append(tmpStr)
+        
+        return resultStr
     }
 }
